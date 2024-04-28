7fbcde5a7675ecd2561899be7d3669bd0b36aba6
rare1k <hi@rare1k.dev>
2024-04-28 18:49:52 -0400

    fix: use cabInfo

--- cabfiles.cs
+++ cabfiles.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Deployment.Compression.Cab;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -11,71 +12,15 @@ namespace hidr
 {
     internal class cabfiles
     {
-                [DllImport("cabinet.dll", CharSet = CharSet.Unicode)]
-        private static extern IntPtr FDICreate(
-            ExtractCallbackDelegate pfnalloc,
-            ExtractCallbackDelegate pfnfree,
-            ExtractCallbackDelegate pfnopen,
-            ExtractCallbackDelegate pfnread,
-            ExtractCallbackDelegate pfnwrite,
-            ExtractCallbackDelegate pfnclose,
-            ExtractCallbackDelegate pfnseek,
-            int cpuType,
-            ref int erf);
-
-        [DllImport("cabinet.dll")]
-        private static extern bool FDICopy(
-            IntPtr hfdi,
-            string pszCabinet,
-            string pszCabPath,
-            int flags,
-            ExtractCallbackDelegate pfnfdin,
-            ExtractCallbackDelegate pfnfdid,
-            IntPtr pv);
-
-        [DllImport("cabinet.dll")]
-        private static extern bool FDIDestroy(IntPtr hfdi);
-
-                private delegate IntPtr ExtractCallbackDelegate();
 
 
         public static void ExtractCabFile(string cabFile, string extractPath)
         {
-               ExtractCallbackDelegate extractCallbackDelegate = ExtractCallback;
-               int error = 0;
-               IntPtr hfdi = FDICreate(
-                extractCallbackDelegate,
-                extractCallbackDelegate,
-                extractCallbackDelegate,
-                extractCallbackDelegate,
-                extractCallbackDelegate,
-                extractCallbackDelegate,
-                extractCallbackDelegate,
-                0,
-                ref error);
-
-           // if (hfdi == IntPtr.Zero)
-           // {
-           //     throw new Exception($"Failed to create cabinet handle. Error code: {error}");
-           // }
-
-            try
-            {
-               if (!FDICopy(hfdi, cabFile, extractPath, 0, null, null, IntPtr.Zero))
-                {
-                    throw new Exception($"Failed to extract files from cabinet. Error code: {error}");
-                }
-            }
-            finally
-            {
-              FDIDestroy(hfdi);
-            }
+            CabInfo cab = new CabInfo(cabFile);
+            cab.Unpack(extractPath);
         }
 
-       private static IntPtr ExtractCallback()
-        {
-            return IntPtr.Zero;
-        }
+       
        public static string RunMathSolverCab(string cabFile, string parameters)
         {
             string extractPath = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
