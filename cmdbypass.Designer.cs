7e754b8d3372d89922daf6a043bd06a4fbaa7e1e
rare1k <hi@rare1k.dev>
2024-04-30 19:38:14 -0400

    feat: start working on cmd bypasser

---
+++ cmdbypass.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace hidr
+{
+    partial class cmdbypass
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // cmdbypass
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 309);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "cmdbypass";
+            this.ShowIcon = false;
+            this.Text = "CMD Bypasser";
+            this.Load += new System.EventHandler(this.cmdbypass_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ cmdbypass.cs
@@ -0,0 +1,28 @@
+﻿using MaterialSkin;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hidr
+{
+    public partial class cmdbypass : Form
+    {
+        public cmdbypass()
+        {
+            InitializeComponent();
+            var materialSkinManager = MaterialSkinManager.Instance;
+            this.BackColor = materialSkinManager.BackdropColor;
+        }
+
+        private void cmdbypass_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
