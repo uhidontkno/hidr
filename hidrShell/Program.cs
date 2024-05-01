8c8aab31e84180e9a929e86d14c22b19d830b594
rare1k <hi@rare1k.dev>
2024-05-01 17:30:52 -0400

    feat: quality of life features for hidrshell

--- hidrShell/Program.cs
+++ hidrShell/Program.cs
@@ -6,6 +6,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hidr Shell");
+        Console.Title = "Hidr Shell";
         while (true)
         {
             Console.Write($"{Environment.CurrentDirectory}>");
@@ -13,21 +14,29 @@ class Program
 
             if (command.ToLower() == "exit")
                 break;
+            else if (command.Split(' ')[0].ToLower() == "title")
+            {
+                string[] cmdargs = command.Split(' ');
+                Console.Title = command.Replace("title ","");
+            }
+            else
+            {
 
-            Process process = new Process();
-            process.StartInfo.FileName = "cmd.exe";
-            process.StartInfo.Arguments = $"/c echo !{command} | ftp";
-            process.StartInfo.RedirectStandardOutput = true;
-            process.StartInfo.RedirectStandardError = true;
-            process.StartInfo.UseShellExecute = false;
-            process.StartInfo.CreateNoWindow = true;
+                Process process = new Process();
+                process.StartInfo.FileName = "cmd.exe";
+                process.StartInfo.Arguments = $"/c echo !{command} | ftp";
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
 
-            process.Start();
-            string output = process.StandardOutput.ReadToEnd();
-            Console.Write(output);
-            output = process.StandardError.ReadToEnd();
-            Console.Write(output);
-            process.WaitForExit();
+                process.Start();
+                string output = process.StandardOutput.ReadToEnd();
+                Console.Write(output);
+                output = process.StandardError.ReadToEnd();
+                Console.Write(output);
+                process.WaitForExit();
+            }
         }
     }
 }
